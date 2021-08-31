de54ca02a8c8d3d3a4e7bc0bacbbe91eb2fdb09a
Jeff Stout <stoutjd@nih.gov>
2021-08-31 15:52:36 -0400

    Initial - pip install code

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Installation
+## Setup Conda Environment
+```#Setup Conda environment
+conda create -n enigma_meg python pip
+conda activate enigma_meg
+```
+## Install python package
+```#Install enigma meg and dependencies
+git clone https://github.com/jstout211/enigma_MEG
+pip install ./enigma_MEG
+```
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nih2mne", 
+    version="0.1",
+    author="Jeff Stout",
+    author_email="stoutjd@nih.gov",
+    description="Adapt coregistration from NIH MEG data for use with MNE python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nih-megcore/nih_to_mne",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: UNLICENSE",
+        "Operating System :: Linux/Unix",
+    ],
+    python_requires='<3.9',
+    install_requires=['mne', 'numpy', 'pytest', 'joblib'],
+    #scripts=['enigmeg/process_meg.py', 
+    #         'enigmeg/process_anatomical.py'],
+)
