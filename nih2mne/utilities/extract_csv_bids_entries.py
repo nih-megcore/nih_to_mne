2166fb8d1c303ebced0c86b0122cce2a5833648d
Jeff Stout <stoutjd@nih.gov>
2022-10-04 17:03:09 -0400

    ENH - writes swarm file from dataframe
    Includes test

--- nih2mne/utilities/extract_csv_bids_entries.py
+++ nih2mne/utilities/extract_csv_bids_entries.py
@@ -10,10 +10,41 @@ import pandas as pd
 import nih2mne
 import os.path as op
 import os 
+import numpy as np
 
 dirpath = op.join(nih2mne.__path__[0], 'templates', 'bids_entry_template.csv')
+#   -bids_dir BIDS_DIR    Output bids_dir path
+#   -meg_input_dir MEG_INPUT_DIR
+#                         'Acquisition directory - typically designated by the acquisition date
+#   -mri_brik MRI_BRIK    Afni coregistered MRI
+#   -mri_bsight MRI_BSIGHT
+#                         Brainsight mri. This should be a .nii file. The exported electrodes text file must be in the same folder and end in .txt. Otherwise, provide the
+#                         mri_sight_elec flag
+#   -mri_bsight_elec MRI_BSIGHT_ELEC
+#                         Exported electrodes file from brainsight. This has the locations of the fiducials
+#   -bids_session BIDS_SESSION
+#                         Data acquisition session. This is set to 1 by default. If the same subject had multiple sessions this must be set manually
+#   -subjid SUBJID 
 
+csv_cmdline_mapping = dict(bids_dir='bids_dir',
+                        meg_dir='meg_input_dir',
+                        mri_brik='mri_brik',
+                        brainsight_mri='mri_bsight',
+                        brainsight_electrodes='mri_bsight_elec',
+                        bids_session='bids_session',
+                        subjid='subjid')
 
+mapping_dtypes = dict(bids_dir=str,
+                        meg_input_dir=str,
+                        mri_brik=str,
+                        mri_bsight=str,
+                        mri_bsight_elec=str,
+                        bids_session='Int16',
+                        subjid=str)
+
+# =============================================================================
+# 
+# =============================================================================
 def find_end_hdr(csvfile):
     i=0
     with open(csvfile) as w:
@@ -35,25 +66,46 @@ def get_version(csvfile):
     return row.split(':')[-1].replace(' ','').replace(',','').replace('\n','')    
 
 
-
 def read_csv_entries(csvfile):
     hdrline = find_end_hdr(csvfile)+1
-    dframe = pd.read_csv(csvfile, skiprows=hdrline)
+    dframe = pd.read_csv(csvfile, skiprows=hdrline, dtype=mapping_dtypes)
+    dframe = dframe.replace(r'^\s*$', np.nan, regex=True)
+    # dframe.fillna(False, inplace=True)
     return dframe
 
 
+def make_cmd(row):
+    '''Assemble a make_meg_bids commandline entry'''
+    tmp=row.copy()
+    tmp.rename(csv_cmdline_mapping, inplace=True) 
+    tmp.dropna(inplace=True) #First round cleaning  
+    cmd = ['make_meg_bids.py']
+    for option,value in tmp.iteritems():
+        cmd.append(f'-{option} {value}')
+    cmd = ' '.join(cmd)
+    return cmd.replace('"','') 
+
+        
+def make_swarm_file(csvfile, swarmfile='megbids_swarm.sh', write=False):
+    dframe = read_csv_entries(csvfile)
+    swarm = []
+    for i,row in dframe.iterrows():
+        cmd = make_cmd(row)
+        swarm.append(cmd)
+    swarm = [i+'\n' for i in swarm]
+    if write==True:
+        with open(swarmfile,'w') as f:
+            f.writelines(swarm)
+    else:
+        return swarm
+        
+    
+    
+
+# def test_
 
 
 
-
-
-# cmdline_mapping = dict(bids_dir='bids_dir',
-#                        meg_input_dir='',
-#                        mri_brik='',
-#                        mri_bsight='',
-#                        mri_bsight_elec='',
-#                        bids_session='',
-#                        subjid='')
 
 
 
--- nih2mne/utilities/tests/test_extract_csv_entries.py
+++ nih2mne/utilities/tests/test_extract_csv_entries.py
@@ -6,20 +6,37 @@ Created on Tue Oct  4 14:50:24 2022
 @author: jstout
 """
 import os.path as op
-from nih2mne.utilities.extract_csv_bids_entries import read_csv_entries, find_end_hdr
+from nih2mne.utilities.extract_csv_bids_entries import (read_csv_entries, 
+                                                        find_end_hdr,
+                                                        make_cmd,
+                                                        make_swarm_file)
+                                                        
 
-
+test_csv_filled=op.join(op.dirname(__file__),'test_entry.csv')
+bids_entry_template = op.join(op.dirname(__file__),'..','..', 'templates', 'bids_entry_template.csv')
 
 def test_find_end_hdr():
-    path=op.join(op.dirname(__file__),'..','..', 'templates', 'bids_entry_template.csv')
-    hdrline=find_end_hdr(path)
+    hdrline=find_end_hdr(bids_entry_template)
     assert hdrline==11
 
 def test_extract_csv():
-    fname=op.join(op.dirname(__file__),'test_entry.csv')
-    dframe = read_csv_entries(fname)
+    dframe = read_csv_entries(test_csv_filled)
     g_truth = ['bids_dir', 'subjid', 'meg_dir', 'afni_brik', 'brainsight_mri',
                'brainsight_electrodes', 'bids_session']
     for i in dframe.columns:
         assert i in g_truth
     assert len(dframe.columns) == len(g_truth)
+
+def test_make_cmd():
+    dframe = read_csv_entries(test_csv_filled)
+    cmd = make_cmd(dframe.loc[0])
+    assert cmd=='make_meg_bids.py -bids_dir bids_dir -subjid test1 -meg_input_dir /data/test -afni_brik /data/mri/mri.BRIK'
+    cmd = make_cmd(dframe.loc[1])
+    assert cmd=='make_meg_bids.py -bids_dir /tmp/test -subjid test2 -meg_input_dir /tmp -mri_bsight /test/tmp.nii -mri_bsight_elec /test/tmp.txt -bids_session 2'
+
+def test_make_swarm_file():
+    swarm=make_swarm_file(test_csv_filled, write=False)
+    assert swarm[0] == 'make_meg_bids.py -bids_dir bids_dir -subjid test1 -meg_input_dir /data/test -afni_brik /data/mri/mri.BRIK\n'
+    assert swarm[1] == 'make_meg_bids.py -bids_dir /tmp/test -subjid test2 -meg_input_dir /tmp -mri_bsight /test/tmp.nii -mri_bsight_elec /test/tmp.txt -bids_session 2\n'
+    
+    
