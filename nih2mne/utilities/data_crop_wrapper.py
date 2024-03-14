0fc8e79c6ea3f838f632a61642eb6181833a73b5
AmaiaBA <amaia.benitezandonegui@nih.gov>
2024-03-14 11:44:37 -0400

    add function to check and rename ClassFile before calling newDs

--- nih2mne/utilities/data_crop_wrapper.py
+++ nih2mne/utilities/data_crop_wrapper.py
@@ -22,6 +22,64 @@ def get_term_time(raw, channel_idx=100):
     except:
         return False
 
+
+def checkClassFile(fname):
+
+    ''' 
+    Load ClassFile.cls. 
+    If "Aborted" class is found in file, 
+    temporarily rename file so newDs does not use it
+    
+    Parameters
+    ----------
+    fname : str
+        Path String.
+
+    '''
+
+    # Open the file
+    with open(os.path.join(fname,'ClassFile.cls'), "r") as file:
+        # Initialize variables to store class information
+        number_of_class = None
+        names = []
+
+        # Read the file line by line
+        for line in file:
+            # Strip whitespace characters from the beginning and end of each line
+            line = line.strip()
+
+            # Check if the line contains "NUMBER OF CLASSES"
+            if line.startswith("NUMBER OF CLASSES:"):
+                # The actual number is on the next line
+                number_of_class = int(next(file).strip())
+            # Check if the line contains "NAME"
+            elif line.startswith("NAME:"):
+                # The actual name is on the next line
+                names.append(next(file).strip())
+                if "Aborted" in names:
+                    to_rename = True
+                # Break the loop if all names are collected
+                if len(names) == number_of_class:
+                    break
+
+    if to_rename:
+        os.rename(os.path.join(fname,'ClassFile.cls'), os.path.join(fname,'__ClassFile.cls'))
+
+
+def renameClassFile(fname):
+
+    ''' 
+    rename '__ClassFile.cls' back to original name after running newDs command
+    
+    Parameters
+    ----------
+    fname : str
+        Path String.
+    
+    '''
+    os.rename(os.path.join(fname,'__ClassFile.cls'), os.path.join(fname,'ClassFile.cls'))
+
+
 def return_cropped_ds(fname):
     '''
     Load the raw dataset, check the time where a set of zeros are present
@@ -55,9 +113,11 @@ def return_cropped_ds(fname):
     f_ = op.basename(fname)
     outdir = op.join(base, 'bids_prep_temp','tmp_cropped')
     if not op.exists(outdir): os.mkdir(outdir)
-    fname_out = op.join(outdir, f_) 
+    fname_out = op.join(outdir, f_)
+    if op.exists(op.join(fname,'ClassFile.cls')): checkClassFile(fname) 
     cmd = f'newDs -f -time 0 {str(crop_time)} {fname} {fname_out}'
     subprocess.run(cmd)
+    if op.exists(op.join(fname,'ClassFile.cls')): renameClassFile(fname) 
     return fname_out
     
 
