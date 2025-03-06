122615535e0394925e336ef0c68bc6273af6edb3
Jeff Stout <stoutjd@nih.gov>
2025-03-06 17:06:05 -0500

    Test added for MEG bids creation

--- nih2mne/tests/test_make_meg_bids.py
+++ nih2mne/tests/test_make_meg_bids.py
@@ -5,6 +5,15 @@ from ..make_meg_bids import _check_multiple_subjects
 from ..make_meg_bids import get_subj_logger
 import logging 
 
+import pytest 
+import nih2mne
+import os.path as op
+from nih2mne.make_meg_bids import process_meg_bids
+code_path = nih2mne.__path__[0]
+data_path = op.join(code_path, 'test_data')
+
+
+
 global logger
 logger = get_subj_logger('TEST', log_dir='/tmp', loglevel=logging.WARN)
 
@@ -69,5 +78,47 @@ def test_sessdir2taskrundict():
          'SUBJ2_M100_20001010_006.ds']
     out_dict2 = sessdir2taskrundict(input_list2, subject_in='TEST')
     assert out_dict2 == g_truth
+    
 
+
+
+def test_process_meg_bids(tmp_path):
+    out_dir = tmp_path / "bids_test_dir"
+    out_dir.mkdir()
+    
+    #Input setup
+    meg_dir = op.join(data_path, '20010101')
+    subject_in = 'ABABABAB'
+    bids_dir = out_dir / 'bids_dir'
+    bids_id = 'S01'
+    session = '1'
+    
+    process_meg_bids(input_path = meg_dir, 
+                     subject_in = subject_in,
+                     bids_dir = bids_dir,
+                     bids_id = bids_id, 
+                     session = session, 
+                     anonymize = False,
+                     ignore_eroom=True, 
+                     crop_trailing_zeros= False
+                     )
+    
+    assert op.exists(bids_dir)
+    for i in ['dataset_description.json','participants.json','participants.tsv','README']:
+        assert op.exists(bids_dir / i)
+    assert op.exists(bids_dir / f'sub-{bids_id}')
+    assert op.exists(bids_dir / f'sub-{bids_id}' / 'ses-1' /'meg')
+    dset_checklist = ['sub-S01_ses-1_task-haririhammer_run-01_meg.json',
+                     'sub-S01_ses-1_task-airpuff_run-01_events.tsv',
+                     'sub-S01_ses-1_task-airpuff_run-01_events.json',
+                     'sub-S01_ses-1_task-airpuff_run-01_meg.ds',
+                     'sub-S01_ses-1_coordsystem.json',
+                     'sub-S01_ses-1_task-haririhammer_run-01_meg.ds',
+                     'sub-S01_ses-1_task-haririhammer_run-01_channels.tsv',
+                     'sub-S01_ses-1_task-airpuff_run-01_channels.tsv',
+                     'sub-S01_ses-1_task-airpuff_run-01_meg.json',
+                     'sub-S01_ses-1_task-haririhammer_run-01_events.tsv',
+                     'sub-S01_ses-1_task-haririhammer_run-01_events.json']
+    for i in dset_checklist:
+        assert op.exists(bids_dir / f'sub-{bids_id}' / 'ses-1' /'meg' / i)
 
