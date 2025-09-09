45ffb72c427bf050fc732fe74e8d5fb4dbe107a5
Jeff Stout <stoutjd@nih.gov>
2025-09-09 17:02:21 -0400

    Minor test for data cropping [skip ci]
    Removing pinning for pyctf which was holding back other dependencies

---
+++ nih2mne/utilities/tests/test_data_crop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  8 16:35:23 2025
+
+@author: jstout
+"""
+
+from nih2mne.utilities.data_crop_wrapper import * 
+import pytest 
+import numpy as np
+
+def test_get_termtime():
+    dat = np.zeros(2000)
+    dat[:1700] = np.random.uniform([1700])
+    idx, t_val = get_term_time(dat, sfreq=100.0)
+    assert idx==1700, 'Did not find the correct stop index'
+    assert t_val==17.0, 'Did not compute the correct stop time'
+
