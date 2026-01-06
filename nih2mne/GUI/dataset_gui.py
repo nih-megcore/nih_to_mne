f21359933d3bf7fd3b8aa3b81d0899dc0e1b5de0
Jeff Stout <stoutjd@nih.gov>
2026-01-06 10:08:35 -0500

    Moved defaults setup to nih2mne/config.py

--- nih2mne/GUI/dataset_gui.py
+++ nih2mne/GUI/dataset_gui.py
@@ -25,6 +25,8 @@ from nih2mne.GUI.templates.input_meg_dset_tile_listWidgetBase import \
 from nih2mne.GUI.templates.input_error_dset_tile_listWidgetBase import \
     Ui_ErrorDatasetTile
 
+from nih2mne.config import TRIG_FILE_LOC
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import pyqtSignal
 import os, os.path as op
@@ -39,49 +41,6 @@ import numpy as np
 from nih2mne.GUI.templates.bids_creator_gui_control_functions \
     import BIDS_MainWindow as BIDS_Ui_MainWindow
 
-
-
-# Eventually move this to the nih2mne import function and reference from there
-def initialize_defaults():
-    # Initialize the locations of the trigger files
-    TRIG_FILE_LOC = op.expanduser(f'~/megcore/trigproc')
-    LOG_FILE_LOC = op.expanduser(f'~/megcore/logs/')
-    DEFAULTS_FILE_LOC = op.expanduser(f'~/megcore/defaults.yml')
-    if not op.exists(TRIG_FILE_LOC):  os.makedirs(TRIG_FILE_LOC)
-    if not op.exists(DEFAULTS_FILE_LOC): 
-        _gen_defaults()
-    defaults = _load_defaults(DEFAULTS_FILE_LOC)
-    return defaults
-
-def _gen_defaults(megcore_dir=op.expanduser(f'~/megcore')):
-    'Write out the default settings into a yml file in the users megcore folder'
-    import yaml
-    BIDS_GEN = {'bids_root': op.expanduser(f'~/BIDS'), 
-                'meg_dir': None, 
-                'mri_dir': None, 
-                'coreg_type': 'Brainsight', 
-                'anonymize': 'N', 
-                'crop_zeros': 'N',
-                'emptyroom': 'N'
-                }
-    
-    
-    defaults = {'BIDS_gen': BIDS_GEN}
-    with open(op.join(megcore_dir, 'defaults.yml'), 'w') as f:
-        yaml.dump(defaults, f, 
-                  sort_keys=False, 
-                  default_flow_style=False)
-
-def _load_defaults(defaults_file=op.expanduser(f'~/megcore/defaults.yml')):
-    'Load the defaults.yml file from the users megcore folder'
-    import yaml 
-    defaults = yaml.safe_load(defaults_file)
-    return defaults
-    
-defaults = initialize_defaults() 
-TRIG_FILE_LOC = op.expanduser(f'~/megcore/trigproc')
-
-#%% 
 
 class GUI_MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
---
+++ nih2mne/config.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  6 09:43:39 2026
+
+@author: jstout
+"""
+import os, os.path as op
+
+
+
+# Eventually move this to the nih2mne import function and reference from there
+def initialize_defaults():
+    # Initialize the locations of the trigger files
+    TRIG_FILE_LOC = op.expanduser(f'~/megcore/trigproc')
+    LOG_FILE_LOC = op.expanduser(f'~/megcore/logs/')
+    DEFAULTS_FILE_LOC = op.expanduser(f'~/megcore/defaults.yml')
+    if not op.exists(TRIG_FILE_LOC):  os.makedirs(TRIG_FILE_LOC)
+    if not op.exists(DEFAULTS_FILE_LOC): 
+        _gen_defaults()
+    defaults = _load_defaults(DEFAULTS_FILE_LOC)
+    return defaults
+
+def _gen_defaults(megcore_dir=op.expanduser(f'~/megcore')):
+    'Write out the default settings into a yml file in the users megcore folder'
+    import yaml
+    BIDS_GEN = {'bids_root': op.expanduser(f'~/BIDS'), 
+                'meg_dir': None, 
+                'mri_dir': None, 
+                'coreg_type': 'Brainsight', 
+                'anonymize': 'N', 
+                'crop_zeros': 'N',
+                'emptyroom': 'N'
+                }
+    
+    
+    defaults = {'BIDS_gen': BIDS_GEN}
+    with open(op.join(megcore_dir, 'defaults.yml'), 'w') as f:
+        yaml.dump(defaults, f, 
+                  sort_keys=False, 
+                  default_flow_style=False)
+
+def _load_defaults(defaults_file=op.expanduser(f'~/megcore/defaults.yml')):
+    'Load the defaults.yml file from the users megcore folder'
+    import yaml 
+    defaults = yaml.safe_load(defaults_file)
+    return defaults
+    
+defaults = initialize_defaults() 
+TRIG_FILE_LOC = op.expanduser(f'~/megcore/trigproc')
