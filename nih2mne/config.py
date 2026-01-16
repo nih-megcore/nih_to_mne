bb2a8d0d66a2de876c89fab7bcac64febcae5ae0
Jeff Stout <stoutjd@nih.gov>
2026-01-16 13:25:22 -0500

    Set precidence to load defaults.yml file from environmental variable

--- nih2mne/config.py
+++ nih2mne/config.py
@@ -31,12 +31,20 @@ def _get_default_options():
     defaults = {'BIDS_gen': BIDS_GEN}    
     return defaults
 
+def _get_defaults_fname():
+    '''Get the defaults filename. Environmental variables take precidence'''
+    if 'MEGCORE_DEFAULTS_FNAME' in os.environ:
+        defaults_fname = os.environ['MEGCORE_DEFAULTS_FNAME']
+    else:
+        megcore_dir=op.expanduser(f'~/megcore')
+        defaults_fname = op.join(megcore_dir, 'defaults.yml')
+    return defaults_fname
 
 def initialize_defaults():
     # Initialize the locations of the trigger files
     TRIG_FILE_LOC = op.expanduser(f'~/megcore/trigproc')
     LOG_FILE_LOC = op.expanduser(f'~/megcore/logs/')
-    DEFAULTS_FILE_LOC = op.expanduser(f'~/megcore/defaults.yml')
+    DEFAULTS_FILE_LOC = _get_defaults_fname()
     if not op.exists(TRIG_FILE_LOC):  os.makedirs(TRIG_FILE_LOC)
     if not op.exists(DEFAULTS_FILE_LOC): 
         _initialize_defaults_file()
@@ -45,19 +53,15 @@ def initialize_defaults():
     defaults = _load_defaults(DEFAULTS_FILE_LOC)
     return defaults
 
-def _initialize_defaults_file(megcore_dir=op.expanduser(f'~/megcore')):
+def _initialize_defaults_file(megcore_dir=_get_defaults_fname()):
     'Write out the default settings into a yml file in the users megcore folder'
     defaults = _get_default_options()
-    with open(op.join(megcore_dir, 'defaults.yml'), 'w') as f:
+    defaults_fname = _get_defaults_fname()
+    with open(defaults_fname, 'w') as f:
         yaml.dump(defaults, f, 
                   sort_keys=False, 
                   default_flow_style=False)
-        
-def _get_defaults_fname():
-    #As a function, this can have a switch to pull from an ENV variable
-    megcore_dir=op.expanduser(f'~/megcore')
-    defaults_fname = op.join(megcore_dir, 'defaults.yml')
-    return defaults_fname
+    print(f'Initialized defaults file to {defaults_fname}')
 
 def _synchronize_defaults_file(input_defaults=None):
     null_defaults = _get_default_options()
@@ -70,9 +74,10 @@ def _synchronize_defaults_file(input_defaults=None):
             need_write = True
         else:
             for minor_key in null_defaults[major_key].keys():
-                if minor_key not in input_defaults.keys():
+                if minor_key not in input_defaults[major_key].keys():
                     input_defaults[major_key][minor_key] = null_defaults[major_key][minor_key]
                     need_write = True
+                    print(f'Added default value entry to defaults file {major_key}[{minor_key}]')
                     
     if need_write == True:
         defaults_fname = _get_defaults_fname()
@@ -82,10 +87,11 @@ def _synchronize_defaults_file(input_defaults=None):
                       default_flow_style=False)
     
 
-def _load_defaults(defaults_file=op.expanduser(f'~/megcore/defaults.yml')):
+def _load_defaults(defaults_file=_get_defaults_fname()):
     '''Load the defaults.yml file from the users megcore folder.
     After loading compare with standard dictionary and write out new options into
     defaults.yml that have been added'''
+    print(f'Using defaults file {defaults_file}')
     with open(defaults_file, 'r') as f:
         defaults = yaml.safe_load(f)
     
