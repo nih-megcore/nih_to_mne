ac4f7534979c8839d3ca7c9cf47c59c3dc741c90
Jeff Stout <stoutjd@nih.gov>
2026-01-26 16:16:19 -0500

    Move prelim testing of new interface to main make_meg_bids test
    Delete uneccesary files

--- nih2mne/tests/test_make_meg_bids.py
+++ nih2mne/tests/test_make_meg_bids.py
@@ -7,6 +7,7 @@ from ..make_meg_bids import convert_brik, make_bids, _gen_taskrundict
 import logging 
 from ..calc_mnetrans import coords_from_afni
 import glob
+from mne_bids import BIDSPath
 
 import nibabel as nib
 import pytest 
@@ -391,6 +392,53 @@ def test_gen_taskrundict():
     assert len(dset_dict['MMFAU'])==2
     assert len(dset_dict['M100'])==3
     
+#%% New interface testing
+from nih2mne.make_meg_bids import _proc_meg_bids, _proc_mri_bids
+
+def test_proc_meg_bids(tmpdir):
+    out_bids_path = op.join(tmpdir, 'BIDS')
+    _bids_path = BIDSPath(subject='TEST', session='1', task='airpuff', 
+                          datatype='meg', suffix='meg', run='01',
+                          root = out_bids_path, extension='.ds'
+                          )
+    
+    meg_fname = str(test_data.meg_airpuff_fname)
+    _proc_meg_bids(meg_fname=meg_fname, bids_path=_bids_path,
+                        anonymize=False, tmpdir=None, ignore_eroom=True, 
+                        crop_trailing_zeros=False, 
+                       )
+    assert _bids_path.fpath.exists()
+
+
+def test_proc_mri_bids(tmpdir):
+    out_bids_path = op.join(tmpdir, 'BIDS')
+    _bids_path = BIDSPath(subject='TEST', session='1',  
+                          datatype='anat', extension='.nii.gz',
+                          root = out_bids_path, suffix='T1w') #, extension='.ds'
+    _bids_path.fpath
+    
+    _proc_mri_bids(t1_bids_path= _bids_path, temp_dir=tmpdir, 
+                   anonymize=False, 
+                   mri_bsight=str(test_data.mri_nii), 
+                   mri_bsight_elec=str(test_data.bsight_elec), 
+                   mri_brik=False, input_id='test')
+    
+    assert op.exists(_bids_path.fpath)
+    json_fname = str(_bids_path.fpath).replace('.nii.gz','.json')
+    assert op.exists(json_fname)
+
+
+# def test_textfill(tmpdir):
+#     'Confirm that anat textline gets filled out'
+#     from nih2mne.GUI.templates.bids_creator_gui_control_functions import BIDS_MainWindow
+#     dsets = ['/fast2/20250205_hv2set/JACTZJMA_rest_20250205_004.ds', '/fast2/20250205_hv2set/JACTZJMA_rest_20250205_007.ds','/fast2/20250205_hv2set/JACTZJMA_MID_20250205_008.ds','/fast2/20250205_hv2set/JACTZJMA_movie_20250205_005.ds']
+    
+#     bmw = BIDS_MainWindow(meg_dsets=dsets)
+#     bmw.ui.list_fname_conversion.addItems(dsets)
+#     bmw._make_task_dict()
+#     bmw.ui.list_fname_conversion.count()
+
+
 
 
 
